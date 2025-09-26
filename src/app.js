4bca81b8c5c08c4570c97509a51b2470b5113f86
b1nhan <sukiakira1411@gmail.com>
2025-09-26 21:39:23 +0700

    add cors config

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 require('dotenv').config(); // Load environment variables
 
 const apiRoutes = require('./routes/api');
@@ -11,10 +12,17 @@ const port = process.env.PORT || 3000;
 // middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
+const vercelDomain = 'https://my-dashboard-b1nhan.vercel.app'; 
+app.use(cors({
+  origin: vercelDomain,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
 
+// route
 app.use('/api', apiRoutes);
 app.use('/api', apiRoutesAuth);
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server is running at http://localhost:${port}`);
+// });
