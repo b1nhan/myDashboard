62ba7e5a095cde0800f6793e39427b62655145ca
b1nhan <sukiakira1411@gmail.com>
2025-09-27 10:11:05 +0700

    change db config/ connection to pool

--- public/config.js
+++ public/config.js
@@ -1,1 +1,4 @@
 window.API_BASE_URL = '__API_URL__';
+
+// local
+// window.API_BASE_URL = 'http://localhost:3000';
--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,24 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,   // max connections
+    queueLimit: 0          // unlimited queued requests
 });
 
 
-db.connect(err => {
-    if (err) {
-        console.error('Error connecting to MySQL:', err.stack);
-        return;
+(async () => {
+    try {
+        const connection = await db.getConnection();
+        console.log('Connected to MySQL database.');
+        connection.release(); // trả connection lại cho pool
+    } catch (err) {
+        console.error('Error connecting to MySQL:', err.message);
     }
-    console.log('Connected to MySQL database.');
-});
+})();
 
-module.exports = db.promise(); // Sử dụng promise để dễ dàng dùng async/await
+module.exports = db; // Sử dụng promise để dễ dàng dùng async/await
